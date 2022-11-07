7e94138b97779429f6fa4f399682e7a9351a43a0
darsmen32@gmail.com <darsmen32@gmail.com>
2022-11-07 12:50:47 -0300

    Spring initializer HU5

---
+++ src/main/java/com/driques/HU5/Hu5Application.java
@@ -0,0 +1,13 @@
+package com.driques.HU5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hu5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hu5Application.class, args);
+	}
+
+}
